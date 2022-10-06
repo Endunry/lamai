4ac52bec0c7435d6b7b4c392a110ba7cc2bbdf3a
Raqhael <Raphael.sack@gmx.de>
2022-10-06 19:10:25 +0200

    moving yellow dot

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import P5 from 'p5';
 
+import Lama from './lama';
+
+const WIDTH = 1000;
+const HEIGHT = 500;
+
+const BACKGROUND = "black";
+
+
+let lama : Lama; 
+
+
 const sketch = (p5: P5) => {
 
     p5.setup = () => {
-        const canvas = p5.createCanvas(200, 200);
+        const canvas = p5.createCanvas(WIDTH, HEIGHT);
         canvas.parent("app");
-        p5.background("gray");
-
+        p5.background(BACKGROUND);
+        lama = new Lama(p5, p5.createVector(100, 100), 50);
     };
 
     p5.draw = () => {
+        p5.background(BACKGROUND);
+        lama.draw();
+        lama.updateDirection();
+        lama.update();
+
     };
 };
 
