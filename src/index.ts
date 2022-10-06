930e0538fb3fff74d4049396ee094acd247ca0ba
Raqhael <Raphael.sack@gmx.de>
2022-10-06 18:57:46 +0200

    p5ts setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import P5 from 'p5';
+
+const sketch = (p5: P5) => {
+
+    p5.setup = () => {
+        const canvas = p5.createCanvas(200, 200);
+        canvas.parent("app");
+        p5.background("gray");
+
+    };
+
+    p5.draw = () => {
+    };
+};
+
+
+new P5(sketch);
